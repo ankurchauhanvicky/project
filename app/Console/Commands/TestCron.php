7b09a94d0bc885cfeffe8c829d3fcd25f423f968
AnkurChauhan <ankurc@whizkraft.net>
2024-03-18 16:47:32 +0530

    jobchange

---
+++ app/Console/Commands/TestCron.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class TestCron extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:cron';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+      $data= array('data'=> 'Cron Testing');
+        Mail::send('mail', $data, function($message){
+            $message->to('ankurc@whizkraft.net')->subject('email testing cron');
+        });
+    }
+}
